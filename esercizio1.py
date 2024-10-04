cccda481f9df8de26652ca717ed4f11d8b151f06
grazzinimirko <mirko.grazzini@students.sarrocchi.it>
2024-10-04 12:42:08 +0200

    Update esercizio1.py

--- esercizio1.py
+++ esercizio1.py
@@ -1,1 +1,16 @@
 class Studente:
+  count=0
+  def __init__(self, nome, eta, matricola)
+    self.nome = nome
+    self.eta = eta
+    self.matricola = matricola
+    Studente.count += 1
+  def visualizza_info(self):
+       return f"Nome: {self.nome}, Età: {self.eta}, Matricola: {self.matricola}"
+
+    @staticmethod
+    def numero_studenti():
+        return Studente.count
+studente1 = Studente("Marco Rossi", 20, "12345")
+studente2 = Studente("Giulia Verdi", 22, "67890")print(studente1.visualizza_info())
+print(studente2.visualizza_info())print(f"Numero totale di studenti: {Studente.numero_studenti()}")
